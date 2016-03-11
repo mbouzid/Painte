bb872df74fbeee3e761dea81e96ae2ae08e8671f
Mariam Bouzid <mariam.bouzid@icloud.com>
2016-03-11 15:20:25 +0100

    Update draw_widget.hpp

--- draw_widget.hpp
+++ draw_widget.hpp
@@ -1,15 +1,16 @@
 #pragma once
 
-#include <QMainWindow>
+#include <QWidget>
+#include <QVBoxLayout>
 #include "draw_area.hpp"
 
-class draw_widget : public QMainWindow
+class draw_widget : public QWidget
 {
  
 private :
 
 	draw_area * m_draw_area ;
-	
+	QVBoxLayout * m_layout ;
 public:
 
     draw_widget()
@@ -18,9 +19,10 @@ public:
     *	@constructor @access public
     **/
     {
+    	m_layout = new QVBoxLayout(this);
     	m_draw_area = new draw_area;
-    	setCentralWidget(m_draw_area);
-
+    	m_layout->addWidget(m_draw_area);
+	
 	}
 
 
